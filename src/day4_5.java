106b1a39af579173076789b71aae1ae377c366c1
am680 <am680@172.16.104.148>
2017-09-25 11:19:11 -0400

    he taught u how to use git

---
+++ src/day4_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day4_5 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int binary = input.nextInt();
+		String binS = Integer.toString(binary);
+		int base = input.nextInt();
+		
+		for(int i = binS.length(); i < 0; i++) {
+			
+		}
+		
+		
+	}
+
+}
