408496fac62b0c82e9fb29c37083c2c8a2097a0a
Rocketdude720 <am680781@yahoo.com>
2019-11-04 14:10:00 -0500

    Merge branch 'master' of C:\Users\am680\IdeaProjects\CompSci with conflicts.

--- APCS/unit2/day7_1.java
+++ APCS/unit2/day7_1.java
@@ -1,64 +1,64 @@
-import java.util.Scanner;
-public class day7_1{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the number of salespeople");
-        SalesPerson[] sales = new SalesPerson[input.nextInt()];
-        for (int i = 0; i < sales.length; i++) {
-            System.out.println("Enter the salesperson's name and salary");
-            sales[i] = new SalesPerson(input.next(), input.nextInt());
-            System.out.println("How many clients does " + sales[i].name + " have?");
-            Client[] c = new Client[input.nextInt];
-            for (int i = 0; i < c.length; i++) {
-                System.out.println("Enter client's name and budget");
-                c[i] = new Client(input.next(), input.nextInt());
-            }
-            sales[i].clients = c;
-        }
-
-        int m = 0;
-        int c = 0;
-        for (int i = 0; i < sales.length; i++) {
-            if(sales[i].clients.length>c)
-                c=sales[i].clients.length; m = i;
-        }
-        System.out.println("The salesperson with the most clients is " + sales[m] + " with " + c + " clients");
-
-        int m = 0;
-        int b = 0;
-        for (int i = 0; i < sales.length; i++) {
-            if(sales[i].getBudg()>b)
-                b=sales[i].getBudg(); m = i;
-        }
-        System.out.println("The salesperson with the most budget is " + sales[m] + " with $" + b );
-    }
-}
-
-class SalesPerson {
-    String name = "";
-    int salary;
-    Client[] clients;
-
-    SalesPerson(String n, int s) {
-        name = n;
-        salary = s;
-    }
-
-    int getBudg() {
-        int sum = 0;
-        for (Client c : clients) {
-            sum+=c.budget;
-        }
-        return(sum);
-    }
-}
-
-class Client {
-    String name;
-    int budget;
-
-    Client(String n, int b) {
-        name = n;
-        budget = b;
-    }
-}
+//import java.util.Scanner;
+//public class day7_1{
+//    public static void main(String[] args){
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the number of salespeople");
+//        SalesPerson[] sales = new SalesPerson[input.nextInt()];
+//        for (int i = 0; i < sales.length; i++) {
+//            System.out.println("Enter the salesperson's name and salary");
+//            sales[i] = new SalesPerson(input.next(), input.nextInt());
+//            System.out.println("How many clients does " + sales[i].name + " have?");
+//            Client[] c = new Client[input.nextInt];
+//            for (int i = 0; i < c.length; i++) {
+//                System.out.println("Enter client's name and budget");
+//                c[i] = new Client(input.next(), input.nextInt());
+//            }
+//            sales[i].clients = c;
+//        }
+//
+//        int m = 0;
+//        int c = 0;
+//        for (int i = 0; i < sales.length; i++) {
+//            if(sales[i].clients.length>c)
+//                c=sales[i].clients.length; m = i;
+//        }
+//        System.out.println("The salesperson with the most clients is " + sales[m] + " with " + c + " clients");
+//
+//        int m = 0;
+//        int b = 0;
+//        for (int i = 0; i < sales.length; i++) {
+//            if(sales[i].getBudg()>b)
+//                b=sales[i].getBudg(); m = i;
+//        }
+//        System.out.println("The salesperson with the most budget is " + sales[m] + " with $" + b );
+//    }
+//}
+//
+//class SalesPerson {
+//    String name = "";
+//    int salary;
+//    Client[] clients;
+//
+//    SalesPerson(String n, int s) {
+//        name = n;
+//        salary = s;
+//    }
+//
+//    int getBudg() {
+//        int sum = 0;
+//        for (Client c : clients) {
+//            sum+=c.budget;
+//        }
+//        return(sum);
+//    }
+//}
+//
+//class Client {
+//    String name;
+//    int budget;
+//
+//    Client(String n, int b) {
+//        name = n;
+//        budget = b;
+//    }
+//}
--- APCS/unit3/Triangle.java
+++ APCS/unit3/Triangle.java
@@ -1,49 +1,49 @@
-public class Triangle {
-
-    private double leg1 = 0;
-    private double leg2 = 0;
-    private double leg3 = 0;
-    private String type = "";
-
-    Triangle(double x, double y){
-        leg1 = x;
-        leg2 = y;
-        leg3 = hypotenuse();
-    }
-
-    double hypotenuse(){
-        return Math.sqrt(Math.pow(leg1, 2) + Math.pow(leg2, 2));
-    }
-
-    public static int similar(Triangle[] set){
-        int count = 0;
-        for (Triangle e : set) {
-            double x = leg1/leg2;
-            double y = e.leg1/e.leg2;
-            if (x==y || x==(1/y))
-                count++;
-        }
-        return count;
-    }
-
-    private String type(){
-        return "";
-    }
-
-    public String getType(){
-        return type;
-    }
-
-    double getLeg1(){
-        return leg1;
-    }
-    double getLeg2(){
-        return leg2;
-    }
-
-    public String toString(){
-        return(leg1 + " by " + leg2);
-    }
-    
-
-}
+//public class Triangle {
+//
+//    private double leg1 = 0;
+//    private double leg2 = 0;
+//    private double leg3 = 0;
+//    private String type = "";
+//
+//    Triangle(double x, double y){
+//        leg1 = x;
+//        leg2 = y;
+//        leg3 = hypotenuse();
+//    }
+//
+//    double hypotenuse(){
+//        return Math.sqrt(Math.pow(leg1, 2) + Math.pow(leg2, 2));
+//    }
+//
+//    public static int similar(Triangle[] set){
+//        int count = 0;
+//        for (Triangle e : set) {
+//            double x = leg1/leg2;
+//            double y = e.leg1/e.leg2;
+//            if (x==y || x==(1/y))
+//                count++;
+//        }
+//        return count;
+//    }
+//
+//    private String type(){
+//        return "";
+//    }
+//
+//    public String getType(){
+//        return type;
+//    }
+//
+//    double getLeg1(){
+//        return leg1;
+//    }
+//    double getLeg2(){
+//        return leg2;
+//    }
+//
+//    public String toString(){
+//        return(leg1 + " by " + leg2);
+//    }
+//
+//
+//}
